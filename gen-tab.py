2ca0aa869d1a34e0ab9c58b82430b68c4a598eb1
LUcas Martins Mendes <lucas.martinsmendes@gmail.com>
2019-11-28 14:34:13 -0300

    update table creatiion scpript

--- gen-tab.py
+++ gen-tab.py
@@ -1,4 +1,6 @@
 import math
 
-for i in range(0,1570):
-    print("when ",i," => return ",math.floor(round(math.sin(i/1000.0)*1000)),";")
+dec_offset = 100.0
+
+for i in range(0,math.floor((math.pi/2)*dec_offset)):
+    print("when ",i," => return ",math.floor(math.sin(i/dec_offset)*dec_offset),";")
